77def713ad3787faf0125cb760efc85e8b9fa388
Dmitriy <larkingit@yandex.ru>
2019-12-24 10:42:37 +0700

    initial development branch commit

--- cloud-server/src/main/java/ServerApp.java
+++ cloud-server/src/main/java/ServerApp.java
@@ -3,6 +3,9 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+/**
+ * Стартуем сервер
+ */
 public class ServerApp {
 
 }
